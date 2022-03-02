0a45f14f012aac1ec654190b68c4ce6d69e430c8
joel1723 <75437050+joel1723@users.noreply.github.com>
2022-03-02 13:06:51 -0500

    people entered

--- assets/app.js
+++ assets/app.js
@@ -2,10 +2,19 @@
 
 
 
+let countEl = document.getElementById("count-el")
+
+
+let count = 0
+
+
 
 
 
 
 function increment() {
+    count = count + 1
+    countEl.innerText = count
 
 }
+increment()
